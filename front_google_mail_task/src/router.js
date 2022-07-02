49953d1051c04119eae5bc27cdfe9dfccb683198
Arthur Chevron <arthur.chevron4@gmail.com>
2022-07-03 01:49:39 +0200

    setup front

---
+++ front_google_mail_task/src/router.js
@@ -0,0 +1,27 @@
+import Vue from "vue"
+import Router from "vue-router"
+Vue.use(Router)
+
+const VueLogin = () => import("./views/Login")
+const VueHome = () => import("./views/Home")
+const Vue404 = () => import("./views/Error404")
+
+// attribution des paths
+const Login = { path: '/', name: 'Login', component: VueLogin }
+const Home = { path: '/browser', name: 'Home', component: VueHome }
+const Error = { path: '/:catchAll(.*)', name: "ErrorPage", component: Vue404}
+
+
+// 1. Définition des routes
+const routes = [
+    Login,
+    Home,
+    /* always at the end */
+    Error
+]
+
+export default new Router({
+    mode: "history",
+    routes,
+    scrollBehavior (){return { x: 0, y: 0 }}
+})
---
+++ front_google_mail_task/src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="login">
+        <p>Please Login</p>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "Login",
+    }
+
+</script>
